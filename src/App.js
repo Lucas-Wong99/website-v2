032552a199d4faf3c67d23d4243c0a7426a9425f
Lucas Wong <lucaswong.ca@gmail.com>
2024-04-01 15:26:19 -0700

    Add the base stuff for project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section>Home</section>
+      <section>About</section>
+      <section>Technical Skills</section>
+      <section>Portfolio 1</section>
+      <section>Portfolio 2</section>
+      <section>Portfolio 3</section>
+      <section>Portfolio 4</section>
+
+      <section>Portfolio 5</section>
+      <section>Footer</section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/NavgationBar/NavigationBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './navigationBar.scss'
+
+const NavigationBar = () => {
+  return <div className="nav-bar"></div>
+}
+
+export default NavigationBar
